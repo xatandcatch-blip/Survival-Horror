b079bf95a0afd1f6dfd15863f9a05cf09e9b83c2
xatandcatch-blip <xatandcatch@gmail.com>
2026-04-10 14:28:25 +0300

    Create SecurityWarning class with warning text
    Added a class to display a security warning message.

---
+++ NodeJava/SecurityWarning.java
@@ -0,0 +1,9 @@
+public class SecurityWarning {
+    public static final String WARNING_TEXT = "WARNING: EXTREME GORE & VIOLENCE. " +
+        "Includes dismemberment and organ removal. " +
+        "Age 18+ Only. Exit immediately if uncomfortable.";
+
+    public static void displayWarning() {
+        // Logic to show this on the Redmi screen before the mansion loads
+    }
+}
